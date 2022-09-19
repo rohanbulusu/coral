6035f40b24a84a18567c435a4a8b9af241277915
Theoden <70821067+rohanbulusu@users.noreply.github.com>
2022-09-19 13:11:29 -0700

    Added Magma and Monoid classes

---
+++ coral/absal/commute.py
@@ -0,0 +1,25 @@
+
+from utils import typename
+from coralset import CoralSet
+from maps import BinaryOperation
+
+# TODO: Test this class
+class Magma:
+
+    def __init__(self, cset, binary_operation):
+        if not isinstance(cset, CoralSet):
+            raise TypeError(f'Expected CoralSet, not {typename(cset)}')
+        if not BinaryOperation.is_binary_operation(binary_operation):
+            raise TypeError(f'Expected a binary operation, not {binary_operation}')
+        self.cset = cset
+        self.binop = binary_operation
+
+# TODO: Test this class
+class Monoid(Magma):
+
+    def __init__(self, cset, identity, binop)
+        super().__init__(cset, binop)
+        if not cset.is_element(identity):
+            raise ValueError(f'Expected identity element {identity} to be contained in the underlying set {cset}')
+        self.identity = identity
+
