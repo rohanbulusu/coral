ead450b20dc86f263d4f080dd1abb1351dee4f30
Theoden <rohanbulusu@gmail.com>
2022-11-24 13:14:16 -0700

    added TestAssociativeOperation, TestCommutativeOperation, TestAbelianOperation, and TestAbelianGroupOperation

--- coral/test_maps.py
+++ coral/test_maps.py
@@ -28,3 +28,64 @@ class TestFunction:
 	def test_rejects_kwarg_inputs(self):
 		with raises(TypeError):
 			assert Function(lambda x: x, [REALS])(1, y=2) == 1
+
+
+class TestAssociativeOperation:
+
+	def test_addition_satisfies_associativity(self):
+		real_addition = AssociativeOperation(lambda a, b: a + b, REALS, REALS)
+		_ = real_addition(1, 2)
+		_ = real_addition(2, 3)
+
+	def test_division_fails_associativity(self):
+		real_division = AssociativeOperation(lambda a, b: a / b, REALS, REALS)
+		_ = real_division(1, 2)
+		with raises(AssociativityError):
+			_ = real_division(3, 4)
+			_ = real_division(5, 6)
+
+
+class TestCommutativeOperation:
+
+	def test_addition_satisfies_commutativity(self):
+		real_addition = CommutativeOperation(lambda a, b: a + b, REALS, REALS)
+		_ = real_addition(1, 2)
+
+	def test_subtraction_fails_commutativity(self):
+		real_subtraction = CommutativeOperation(lambda a, b: a - b, REALS, REALS)
+		with raises(CommutativityError):
+			_ = real_subtraction(1, 2)
+
+
+class TestAbelianOperation:
+
+	def test_addition_is_abelian(self):
+		real_addition = AbelianOperation(lambda a, b: a + b, REALS, REALS)
+		_ = real_addition(1, 2)
+
+	def test_subtraction_is_not_abelian(self):
+		real_subtraction = AbelianOperation(lambda a, b: a - b, REALS, REALS)
+		with raises(CommutativityError):
+			_ = real_subtraction(1, 2)
+
+
+class TestAbelianGroupOperation:
+
+	def test_addition_is_abelian_group_operation(self):
+		real_addition = AbelianGroupOperation(lambda a, b: a + b, REALS, REALS)
+		_ = real_addition(1, 2)
+
+	def test_subtraction_fails_commutativity(self):
+		real_subtraction = AbelianGroupOperation(lambda a, b: a - b, REALS, REALS)
+		with raises(CommutativityError):
+			_ = real_subtraction(1, 2)
+
+	def test_operation_fails_associativity(self):
+		operation = AbelianGroupOperation(lambda a, b: 2*a + 2*b, REALS, REALS)
+		_ = operation(1, 2)
+		with raises(AssociativityError):
+			_ = operation(3, 4)
+			_ = operation(5, 6)
+
+
+
