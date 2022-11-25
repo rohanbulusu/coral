705e646fea355cf3c1b2fad061ac961b5e4f96b3
Theoden <rohanbulusu@gmail.com>
2022-11-24 19:09:21 -0700

    made is_commutative a non-static method

--- coral/maps.py
+++ coral/maps.py
@@ -111,20 +111,17 @@ class AssociativeOperation(ClosedOperation):
 
 class CommutativeOperation(ClosedOperation):
 
-	@staticmethod
-	def is_commutative(candidate, a, b):
-		if not isinstance(candidate, ClosedOperation):
-			return False
-		if not all(candidate.domain.has_element(sample) for sample in (a, b)):
+	def is_commutative(self, a, b):
+		if not all(self.domain.has_element(sample) for sample in (a, b)):
 			raise DomainError(f'Not all sample elements are in the binary operation\'s domain')
-		if not candidate._func(a, b) == candidate._func(b, a):
+		if not self._func(a, b) == self._func(b, a):
 			return False
 		return True
 
 	def __call__(self, a, b):
 		result = super().__call__(a, b)
 		if self.num_samples >= 2:
-			if not self.is_commutative(self, *unique_choices(list(self.cached_samples), 2)):
+			if not self.is_commutative(*unique_choices(list(self.cached_samples), 2)):
 				raise CommutativityError('Operation is not commutative over the given domain')
 		return result
 
