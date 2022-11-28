6436ddea82c1cd2337c8f337760cefbd31633d9c
Theoden <70821067+rohanbulusu@users.noreply.github.com>
2022-11-28 11:04:08 -0700

    Implemented a rudimentary ring structure

--- coral/absal/rings.py
+++ coral/absal/rings.py
@@ -1,7 +1,37 @@
 
 from coral.utils import typename
 from coral.coralset import CoralSet
-from coral.maps import BinaryOperation
+from coral.maps import PropertyError, AssociativeOperation, LeftInvertibleOperation, RightInvertibleOperation
+from .groups import AbelianGroupOperation
 
 class Ring:
-    ...
+    
+    def __init__(self, cset, group_addition, multiplication):
+        if not isinstance(cset, CoralSet):
+            raise TypeError(f'Expected CoralSet, not {typename(cset)}')
+        if not isinstance(group_addition, AbelianGroupOperation):
+            raise PropertyError(f'Expected an abelian group addition, not {typename(group_addition)}')
+        if not isinstance(multiplication, AssociativeOperation):
+            raise PropertyError(f'Expected an associative multiplication, not {typename(multiplication}')
+        self.cset = cset
+        self.addition = group_addition
+        self.multiplication = multiplication
+    
+    def add(self, a, b):
+        return self.addition(a, b)
+    
+    def mul(self, a, b):
+        return self.multiplication(a, b)
+
+
+class Ideal:
+
+    def __init__(self, ring, ideal_elements):
+        self.ring = ring
+        self.cset = ideal_elements
+
+    
+
+
+
+
