ed89429dd791ac5c0d7abe24994cfa0a33e3b059
Theoden <rohanbulusu@gmail.com>
2022-12-04 15:31:50 -0700

    added tests for new CoralSet attributes specifications for CoralSet.is_subset and CoralSet.has_subset

--- coral/test_coralset.py
+++ coral/test_coralset.py
@@ -100,3 +100,73 @@ class TestCoralSet:
         assert 0 in CoralSet(int)
         assert 1j in CoralSet(complex)
         assert 0.01 in CoralSet(float)
+
+    def test_is_subset_for_discrete_subsets_of_an_infinite_set(self, R):
+        assert CoralSet((1, 2, 3)).is_subset(R)
+        assert CoralSet((-3, -2, -1)).is_subset(R)
+        assert CoralSet((-1, 0, 1)).is_subset(R)
+
+    def test_is_subset_for_infinite_subsets_of_an_infinite_set(self, Z, R, C):
+        assert R.is_subset(C)
+        assert CoralSet(float).is_subset(C)
+        assert Z.is_subset(R)
+
+        R_adjoin_i = R | CoralSet((1j, -1j))
+        assert R.is_subset(R_adjoin_i)
+        assert R_adjoin_i.is_subset(R)
+        assert R_adjoin_i.is_subset(C)
+
+    def test_is_subset_for_discrete_subsets_of_a_discrete_set(self):
+        A = CoralSet((1, 2, 3))
+        sub_A = CoralSet((1, 2))
+        assert sub_A.is_subset(A)
+
+        B = A | CoralSet((4, 5, 6))
+        sub_B = CoralSet(5, 6)
+        assert sub_B.is_subset(B)
+        assert A.is_subset(B)
+        assert sub_A.is_subset(B)
+
+    def test_is_subset_for_invalidity_of_infinite_subsets_of_a_discrete_set(self, R, C):
+        discrete = CoralSet((1, 2, 3))
+        assert not R.is_subset(discrete)
+        assert not C.is_subset(discrete)
+
+    def test_is_subset_is_proper_subset(self, R, C):
+        assert not R.is_subset(R)
+        assert not CoralSet((1, 2, 3)).is_subset(CoralSet((1, 2, 3)))
+
+    def test_has_subset_for_discrete_subsets_of_an_infinite_set(self, R):
+        assert R.has_subset(CoralSet((1, 2, 3)))
+        assert R.has_subset(CoralSet((-1, -2, - 3)))
+        assert R.has_subset(CoralSet((-1, 0, 1)))
+
+    def test_has_subset_for_infinite_subsets_of_an_infinite_set(self, Z, R, C):
+        assert C.has_subset(R)
+        assert C.has_subset(CoralSet(float))
+        assert R.has_subset(Z)
+
+        R_adjoin_i = R | CoralSet((1j, -1j))
+        assert R_adjoin_i.has_subset(R)
+        assert R.has_subset(R_adjoin_i)
+        assert C.has_subset(R_adjoin_i)
+
+    def test_has_subset_for_discrete_subsets_of_a_discrete_set(self):
+        A = CoralSet((1, 2, 3))
+        sub_A = CoralSet((1, 2))
+        assert A.has_subset(sub_A)
+
+        B = A | CoralSet((4, 5, 6))
+        sub_B = CoralSet(5, 6)
+        assert B.has_subset(sub_B)
+        assert B.has_subset(A)
+        assert B.has_subset(sub_A)
+
+    def test_has_subset_for_invalidity_of_infinite_subsets_of_a_discrete_set(self, R, C):
+        discrete = CoralSet((1, 2, 3))
+        assert not discrete.has_subset(R)
+        assert not discrete.has_subset(C)
+
+    def test_has_subset_is_proper_subset(self, R, C):
+        assert not R.has_subset(R)
+        assert not CoralSet((1, 2, 3)).has_subset(CoralSet((1, 2, 3)))
